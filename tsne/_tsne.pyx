51bad0940d39831e94b6d00c280a5078d9dcb940
Pavlin Poličar <pavlin.g.p@gmail.com>
2018-06-18 12:11:36 +0200

    Embed generating vector in 1d case in multiply function, to be consistent with 2d case

--- tsne/_tsne.pyx
+++ tsne/_tsne.pyx
@@ -362,21 +362,16 @@ cpdef double estimate_negative_gradient_fft_1d(
     # coefficients
     cdef double[:, ::1] interpolated_values = interpolate(y_in_box, y_tilde_spacing)
 
-    cdef double[:, ::1] w_coeffs = np.zeros((total_interpolation_points, n_terms), dtype=float)
+    cdef double[:, ::1] w_coefficients = np.zeros((total_interpolation_points, n_terms), dtype=float)
     for i in range(N):
         box_idx = point_box_idx[i] * n_interpolation_points
         for j in range(n_interpolation_points):
             for d in range(n_terms):
-                w_coeffs[box_idx + j, d] += interpolated_values[i, j] * charges_Qij[i, d]
-
-    cdef double[:, ::1] embedded_w_coeffs = np.zeros((2 * total_interpolation_points, n_terms), dtype=float)
-    for i in range(total_interpolation_points):
-        for d in range(n_terms):
-            embedded_w_coeffs[total_interpolation_points + i, d] = w_coeffs[i, d]
+                w_coefficients[box_idx + j, d] += interpolated_values[i, j] * charges_Qij[i, d]
 
     # Step 2: Compute the values v_{m, n} at the equispaced nodes, multiply the
     # kernel matrix with the coefficients w
-    cdef double[:, ::1] y_tilde_values = matrix_multiply_fft_1d(kernel_tilde, embedded_w_coeffs)
+    cdef double[:, ::1] y_tilde_values = matrix_multiply_fft_1d(kernel_tilde, w_coefficients)
 
     # Step 3: Compute the potentials \tilde{\phi}
     cdef double[:, ::1] potentials = np.zeros((N, n_terms), dtype=float)
@@ -403,18 +398,46 @@ cpdef double estimate_negative_gradient_fft_1d(
 
 cdef double[:, ::1] matrix_multiply_fft_1d(
     double[::1] kernel_tilde,
-    double[:, ::1] embedded_w_coeffs,
+    double[:, ::1] w_coefficients,
 ):
+    """Multiply the the kernel vectr K tilde with the w coefficients.
+    
+    Parameters
+    ----------
+    kernel_tilde : memoryview
+        The generating vector of the 2d Toeplitz matrix i.e. the kernel 
+        evaluated all all interpolation points from the left most 
+        interpolation point, embedded in a circulant matrix (doubled in size 
+        from (n_interp, n_interp) to (2 * n_interp, 2 * n_interp) and 
+        symmetrized. See how to embed Toeplitz into circulant matrices.
+    w_coefficients : memoryview
+        The coefficients calculated in Step 1 of the paper, a
+        (n_total_interp, n_terms) matrix. The coefficients are embedded into a
+        larger matrix in this function, so no prior embedding is needed.
+        
+    Returns
+    -------
+    memoryview
+        The output. Contains the evaluated kernel values at the equspaced 
+        interpolation nodes.
+    
+    """
     cdef:
-        Py_ssize_t total_interpolation_points = embedded_w_coeffs.shape[0]
-        Py_ssize_t n_terms = embedded_w_coeffs.shape[1]
+        Py_ssize_t n_interpolation_points_1d = w_coefficients.shape[0]
+        Py_ssize_t n_terms = w_coefficients.shape[1]
         Py_ssize_t n_fft_coeffs = kernel_tilde.shape[0]
 
-        double[:, ::1] y_tilde_values = np.empty((total_interpolation_points, n_terms), dtype=float)
+        double[:, ::1] y_tilde_values = np.empty((n_interpolation_points_1d, n_terms), dtype=float)
 
-        complex[::1] fft_kernel_tilde = np.empty(2 * total_interpolation_points, dtype=complex)
+        complex[::1] fft_kernel_tilde = np.empty(n_fft_coeffs, dtype=complex)
         complex[::1] fft_w_coeffs = np.empty(n_fft_coeffs, dtype=complex)
-        double[::1] fft_io_buffer = np.empty(n_fft_coeffs, dtype=float)
+        # Note that we can't use the same buffer for the input and output since
+        # we only write to the first half of the vector - we'd need to
+        # manually zero out the rest of the entries that were inevitably
+        # changed during the IDFT, so it's faster to use two buffers, at the
+        # cost of some memory
+        double[::1] fft_in_buffer = np.zeros(n_fft_coeffs, dtype=float)
+        double[::1] fft_out_buffer = np.zeros(n_fft_coeffs, dtype=float)
 
         Py_ssize_t d, i
 
@@ -430,18 +453,18 @@ cdef double[:, ::1] matrix_multiply_fft_1d(
 
     plan_dft = fftw_plan_dft_r2c_1d(
         n_fft_coeffs,
-        &fft_io_buffer[0], <fftw_complex *>(&fft_w_coeffs[0]),
+        &fft_in_buffer[0], <fftw_complex *>(&fft_w_coeffs[0]),
         FFTW_ESTIMATE,
     )
     plan_idft = fftw_plan_dft_c2r_1d(
         n_fft_coeffs,
-        <fftw_complex *>(&fft_w_coeffs[0]), &fft_io_buffer[0],
+        <fftw_complex *>(&fft_w_coeffs[0]), &fft_out_buffer[0],
         FFTW_ESTIMATE,
     )
 
     for d in range(n_terms):
-        for i in range(n_fft_coeffs):
-            fft_io_buffer[i] = embedded_w_coeffs[i, d]
+        for i in range(n_interpolation_points_1d):
+            fft_in_buffer[i] = w_coefficients[i, d]
 
         fftw_execute(plan_dft)
 
@@ -451,11 +474,11 @@ cdef double[:, ::1] matrix_multiply_fft_1d(
 
         fftw_execute(plan_idft)
 
-        for i in range(total_interpolation_points):
+        for i in range(n_interpolation_points_1d):
             # FFTW doesn't perform IDFT normalization, so we have to do it
             # ourselves. This is done by multiplying the result with the number
             #  of points in the input
-            y_tilde_values[i, d] = fft_io_buffer[i].real / (total_interpolation_points * 2)
+            y_tilde_values[i, d] = fft_out_buffer[n_interpolation_points_1d + i].real / n_fft_coeffs
 
     fftw_destroy_plan(plan_dft)
     fftw_destroy_plan(plan_idft)
@@ -500,11 +523,11 @@ cdef double[:, ::1] matrix_multiply_fft_2d(
         fftw_plan plan_dft, plan_idft
         complex[::1] fft_w_coefficients = np.empty(n_fft_coeffs * (n_fft_coeffs / 2 + 1), dtype=complex)
         complex[::1] fft_kernel_tilde = np.empty(n_fft_coeffs * (n_fft_coeffs / 2 + 1), dtype=complex)
-        # Note that we can't use the same buffer for the input and output like
-        # in the 1d case, since we only write to the top quadrant of the in
-        # matrix - we'd need to manually zero out the rest of the entries that
-        # were inevitably changed during the IDFT, so it's faster to use two
-        # buffers, at the cost of some memory
+        # Note that we can't use the same buffer for the input and output since
+        # we only write to the top quadrant of the in matrix - we'd need to
+        # manually zero out the rest of the entries that were inevitably
+        # changed during the IDFT, so it's faster to use two buffers, at the
+        # cost of some memory
         double[:, ::1] fft_in_buffer = np.zeros((n_fft_coeffs, n_fft_coeffs))
         double[:, ::1] fft_out_buffer = np.zeros((n_fft_coeffs, n_fft_coeffs))
 
