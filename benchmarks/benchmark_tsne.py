9bbfa00863b3af8ee013b6159c9e9678bfa8a3a2
Pavlin Poličar <pavlin.g.p@gmail.com>
2018-06-22 20:49:01 +0200

    Refactor benchmark to be more usable

--- benchmarks/benchmark_tsne.py
+++ benchmarks/benchmark_tsne.py
@@ -21,6 +21,7 @@ FILE_DIR = dirname(abspath(__file__))
 DATA_DIR = join(FILE_DIR, 'data')
 
 np.set_printoptions(precision=4, suppress=True)
+np.random.seed(42)
 
 
 def plot(x: np.ndarray, y: np.ndarray, show: bool = True, **kwargs) -> None:
@@ -44,7 +45,7 @@ def plot1d(x: np.ndarray, y: np.ndarray, show: bool = True, **kwargs) -> None:
         plt.show()
 
 
-def get_mnist_full():
+def get_mnist(n_samples=None):
     if not os.path.exists('data/mnist.pkl.gz'):
         urllib.request.urlretrieve(
             'http://deeplearning.net/data/mnist/mnist.pkl.gz', 'data/mnist.pkl.gz')
@@ -54,10 +55,25 @@ def get_mnist_full():
     _train = np.asarray(train[0], dtype=np.float64)
     _val = np.asarray(val[0], dtype=np.float64)
     _test = np.asarray(test[0], dtype=np.float64)
-    mnist = np.vstack((_train, _val, _test))
-    classes = np.hstack((train[1], val[1], test[1]))
+    x = np.vstack((_train, _val, _test))
+    y = np.hstack((train[1], val[1], test[1]))
 
-    return mnist, classes
+    if n_samples is not None:
+        indices = np.random.choice(list(range(x.shape[0])), 20000, replace=False)
+        x, y = x[indices], y[indices]
+
+    return x, y
+
+
+def get_mouse_60k():
+    with open(join(DATA_DIR, 'sc-mouse-60k-1k.pkl'), 'rb') as f:
+        x = pickle.load(f)
+    x = x.astype(np.float32).toarray()
+    import json
+    with open(join(DATA_DIR, 'sc-mouse-60k-1k-y.txt'), 'rb') as f:
+        y = np.array(json.load(f), dtype=int)
+
+    return x, y
 
 
 def tmp():
@@ -79,19 +95,7 @@ def tmp():
 
 
 def run():
-    # mnist = datasets.load_digits()
-    # x, y = mnist['data'], mnist['target']
-    # np.random.seed(1)
-    # x, y = get_mnist_full()
-    # indices = np.random.choice(list(range(x.shape[0])), 5000, replace=False)
-    # x, y = x[indices], y[indices]
-
-    with open(join(DATA_DIR, 'sc-mouse-60k-1k.pkl'), 'rb') as f:
-        x = pickle.load(f)
-    x = x.astype(np.float32).toarray()
-    import json
-    with open(join(DATA_DIR, 'sc-mouse-60k-1k-y.txt'), 'rb') as f:
-        y = np.array(json.load(f), dtype=int)
+    x, y = get_mouse_60k()
 
     angle = 0.5
     perplexity = 30
@@ -117,19 +121,6 @@ def run():
     plt.title('tsne')
     plot(embedding, y)
 
-    init = PCA(n_components=2).fit_transform(x)
-    start = time.time()
-    embedding = MulticoreTSNE(
-        early_exaggeration=ee, learning_rate=lr, perplexity=perplexity,
-        n_jobs=threads, cheat_metric=False, angle=angle, init=init,
-        metric=metric, verbose=True
-    ).fit_transform(x)
-    print('-' * 80)
-    print('mctsne', time.time() - start)
-    plt.title('mctsne')
-    plot(embedding, y)
-    plt.show()
-
     x = np.ascontiguousarray(x.astype(np.float64))
     from fitsne import FItSNE
     start = time.time()
@@ -140,6 +131,20 @@ def run():
     print('-' * 80)
     print('fft interp %.4f' % (time.time() - start))
     plt.title('fft interp')
+    plot(embedding, y)
+    plt.show()
+    return
+
+    init = PCA(n_components=2).fit_transform(x)
+    start = time.time()
+    embedding = MulticoreTSNE(
+        early_exaggeration=ee, learning_rate=lr, perplexity=perplexity,
+        n_jobs=threads, cheat_metric=False, angle=angle, init=init,
+        metric=metric, verbose=True
+    ).fit_transform(x)
+    print('-' * 80)
+    print('mctsne', time.time() - start)
+    plt.title('mctsne')
     plot(embedding, y)
     plt.show()
 
@@ -155,19 +160,17 @@ def run():
     plt.show()
 
 
-def transform(n_jobs=4):
-    iris = datasets.load_iris()
-    x = iris['data']
-    y = iris['target']
+def transform(n_jobs=4, grad='bh', neighbors='approx'):
+    x, y = get_mnist(20000)
 
     x_train, x_test, y_train, y_test = train_test_split(
-        x, y, test_size=0.33, random_state=42)
+        x, y, test_size=0.75, random_state=42)
 
     tsne = TSNE(
         perplexity=30, learning_rate=100, early_exaggeration=12,
         n_jobs=n_jobs, theta=0.5, initialization='pca', metric='euclidean',
-        n_components=2, n_iter=750, early_exaggeration_iter=250, neighbors='exact',
-        negative_gradient_method='bh', min_num_intervals=10, ints_in_interval=2,
+        n_components=2, n_iter=750, early_exaggeration_iter=250, neighbors=neighbors,
+        negative_gradient_method=grad, min_num_intervals=10, ints_in_interval=2,
         late_exaggeration_iter=0, late_exaggeration=4,
     )
     start = time.time()
@@ -178,9 +181,9 @@ def transform(n_jobs=4):
     plot(embedding, y_train, show=False, ms=3)
 
     start = time.time()
-    partial_embedding = embedding.transform(
-        x_test, perplexity=10, early_exaggeration_iter=0,
-        final_momentum=0.2, initialization='random')
+    partial_embedding = embedding.get_partial_embedding_for(
+        x_test, perplexity=20, initialization='random')
+    partial_embedding.optimize(200, exaggeration=4, inplace=True, momentum=0.4)
     print('tsne trasnsform', time.time() - start)
 
     plt.subplot(122)
