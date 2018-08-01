ef2114c33708ffe92b4d728465b3db68b9b28ed7
Pavlin Poličar <pavlin.g.p@gmail.com>
2018-08-01 19:02:21 +0200

    Update package name to something usable on pypi

--- setup.py
+++ setup.py
@@ -8,17 +8,17 @@ USE_CYTHON = os.environ.get('USE_CYTHON', False)
 ext = 'pyx' if USE_CYTHON else 'c'
 
 extensions = [
-    Extension('tsne.quad_tree', ['tsne/quad_tree.%s' % ext],
+    Extension('fastTSNE.quad_tree', ['fastTSNE/quad_tree.%s' % ext],
               extra_compile_args=['-fopenmp', '-O3'],
               extra_link_args=['-fopenmp', '-O3'],
               include_dirs=[np.get_include()],
               ),
-    Extension('tsne._tsne', ['tsne/_tsne.%s' % ext],
+    Extension('fastTSNE._tsne', ['fastTSNE/_tsne.%s' % ext],
               extra_compile_args=['-fopenmp', '-lfftw3', '-O3'],
               extra_link_args=['-fopenmp', '-lfftw3', '-O3'],
               include_dirs=[np.get_include()],
               ),
-    Extension('tsne.kl_divergence', ['tsne/kl_divergence.%s' % ext],
+    Extension('fastTSNE.kl_divergence', ['fastTSNE/kl_divergence.%s' % ext],
               extra_compile_args=['-fopenmp', '-O3'],
               extra_link_args=['-fopenmp', '-O3'],
               include_dirs=[np.get_include()],
@@ -32,11 +32,11 @@ if USE_CYTHON:
 print(extensions)
 
 setup(
-    name='t-SNE',
+    name='fastTSNE',
     description='',
     author='Pavlin Poličar',
     author_email='pavlin.g.p@gmail.com',
-    version='0.1.1',
+    version='0.1.4',
     url='https://github.com/pavlin-policar/tSNE',
     packages=setuptools.find_packages(),
     ext_modules=extensions,
