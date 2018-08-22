d943a630c91f5d47869d4175ab3dd385b12ae60b
Pavlin Poličar <pavlin.g.p@gmail.com>
2018-08-22 10:37:09 +0200

    Version bump to 0.1.10

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     description='',
     author='Pavlin Poličar',
     author_email='pavlin.g.p@gmail.com',
-    version='0.1.9',
+    version='0.1.10',
     url='https://github.com/pavlin-policar/fastTSNE',
     packages=setuptools.find_packages(),
     ext_modules=extensions,
