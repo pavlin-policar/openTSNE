aff51cf60bb27436b933ba23f04201e620c4ed0c
Pavlin Poličar <pavlin.g.p@gmail.com>
2018-09-13 09:09:16 +0200

    Add tSNE test on iris

--- tests/test_correctness.py
+++ tests/test_correctness.py
@@ -1,6 +1,9 @@
 import unittest
 
 import numpy as np
+from sklearn import datasets
+from sklearn.metrics import accuracy_score
+from sklearn.neighbors import KNeighborsClassifier
 
 from fastTSNE.callbacks import VerifyExaggerationError
 from fastTSNE.tsne import TSNE, TSNEEmbedding
@@ -48,3 +51,27 @@ class TestTSNECorrectness(unittest.TestCase):
             50, exaggeration=5, callbacks=[VerifyExaggerationError(embedding)],
             callbacks_every_iters=1, inplace=True,
         )
+
+    def test_iris(self):
+        iris = datasets.load_iris()
+        x, y = iris['data'], iris['target']
+
+        # Evaluate tSNE optimization using a KNN classifier
+        knn = KNeighborsClassifier(n_neighbors=5)
+        tsne = TSNE(perplexity=30, initialization='random')
+
+        # Prepare a random initialization
+        embedding = tsne.prepare_initial(x)
+
+        # KNN should do poorly on a random initialization
+        knn.fit(embedding, y)
+        predictions = knn.predict(embedding)
+        self.assertTrue(accuracy_score(predictions, y) < .5)
+
+        # Optimize the embedding for a small number of steps so tests run fast
+        embedding.optimize(50, inplace=True)
+
+        # Similar points should be grouped together, therefore KNN should do well
+        knn.fit(embedding, y)
+        predictions = knn.predict(embedding)
+        self.assertTrue(accuracy_score(predictions, y) > .95)
