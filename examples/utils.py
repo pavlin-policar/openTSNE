0030ccf516c1ff279ec19c58ee82340e736aa9b0
Pavlin Poličar <pavlin.g.p@gmail.com>
2018-10-21 16:20:54 +0200

    Add notebook for animation

--- examples/utils.py
+++ examples/utils.py
@@ -1,19 +1,42 @@
-from os.path import abspath, dirname, join
+import gzip
+import os
 import pickle
-import numpy as np
+import urllib
+from os.path import abspath, dirname, join
 
+import numpy as np
 
 FILE_DIR = dirname(abspath(__file__))
 DATA_DIR = join(FILE_DIR, 'data')
 
 
 def get_zeisel_2018(n_samples: int = None):
-    with open(join(DATA_DIR, 'zeisel_2018.pickle'), 'rb') as f:
+    with gzip.open(join(DATA_DIR, 'zeisel_2018.pkl.gz'), 'rb') as f:
         data = pickle.load(f)
     
     # Extract log normalized counts and cell type
     x, y = data['log_counts'], data['CellType1']
     x = x.T
+
+    if n_samples is not None:
+        indices = np.random.choice(list(range(x.shape[0])), n_samples, replace=False)
+        x, y = x[indices], y[indices]
+
+    return x, y
+
+
+def get_mnist(n_samples: int = None):
+    if not os.path.exists(join(DATA_DIR, 'mnist.pkl.gz')):
+        urllib.request.urlretrieve(
+            'http://deeplearning.net/data/mnist/mnist.pkl.gz', join(DATA_DIR, 'mnist.pkl.gz'))
+
+    with gzip.open(join(DATA_DIR, 'mnist.pkl.gz'), 'rb') as f:
+        train, val, test = pickle.load(f, encoding='latin1')
+    _train = np.asarray(train[0], dtype=np.float64)
+    _val = np.asarray(val[0], dtype=np.float64)
+    _test = np.asarray(test[0], dtype=np.float64)
+    x = np.vstack((_train, _val, _test))
+    y = np.hstack((train[1], val[1], test[1]))
 
     if n_samples is not None:
         indices = np.random.choice(list(range(x.shape[0])), n_samples, replace=False)
