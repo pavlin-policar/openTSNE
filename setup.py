ab022e6ff4e577f52bf483d15c992272eea282f9
Pavlin Poličar <pavlin.g.p@gmail.com>
2018-10-25 13:32:48 +0200

    Version bump to 0.2.8

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     license='BSD-3-Clause',
     author='Pavlin Poličar',
     author_email='pavlin.g.p@gmail.com',
-    version='0.2.7',
+    version='0.2.8',
     url='https://github.com/pavlin-policar/fastTSNE',
     packages=setuptools.find_packages(),
     ext_modules=extensions,
