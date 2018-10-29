9e31faa0acdf9627611664fc49a585cfabdba117
Pavlin Poličar <pavlin.g.p@gmail.com>
2018-10-29 18:51:29 +0100

    Add multiscale kernel affinities

--- fastTSNE/affinity.py
+++ fastTSNE/affinity.py
@@ -86,20 +86,20 @@ class PerplexityBasedNN(Affinities):
 
         self.perplexity = perplexity
         self.knn_index = knn_index
-        self.P = joint_probabilities_nn(
-            neighbors, distances, perplexity, symmetrize=symmetrize, n_jobs=n_jobs)
+        self.P = joint_probabilities_nn(neighbors, distances, [perplexity],
+                                        symmetrize=symmetrize, n_jobs=n_jobs)
 
         self.n_jobs = n_jobs
 
     def to_new(self, data, perplexity=None, return_distances=False):
-        perplexity = perplexity or self.perplexity
+        perplexity = perplexity if perplexity is not None else self.perplexity
         perplexity = self.check_perplexity(perplexity)
         k_neighbors = min(self.n_samples - 1, int(3 * perplexity))
 
         neighbors, distances = self.knn_index.query(data, k_neighbors)
 
         P = joint_probabilities_nn(
-            neighbors, distances, perplexity, symmetrize=False,
+            neighbors, distances, [perplexity], symmetrize=False,
             n_reference_samples=self.n_samples, n_jobs=self.n_jobs,
         )
 
@@ -112,8 +112,8 @@ class PerplexityBasedNN(Affinities):
         """Check for valid perplexity value."""
         if self.n_samples - 1 < 3 * perplexity:
             old_perplexity, perplexity = perplexity, (self.n_samples - 1) / 3
-            log.warning('Perplexity value %d is too high. Using perplexity %.2f' %
-                        (old_perplexity, perplexity))
+            log.warning('Perplexity value %d is too high. Using perplexity '
+                        '%.2f instead' % (old_perplexity, perplexity))
 
         return perplexity
 
@@ -139,7 +139,7 @@ def build_knn_index(data, method, metric, metric_params=None, n_jobs=1, random_s
     return knn_index
 
 
-def joint_probabilities_nn(neighbors, distances, perplexity, symmetrize=True,
+def joint_probabilities_nn(neighbors, distances, perplexities, symmetrize=True,
                            n_reference_samples=None, n_jobs=1):
     """Compute the conditional probability matrix P_{j|i}.
 
@@ -147,22 +147,22 @@ def joint_probabilities_nn(neighbors, distances, perplexity, symmetrize=True,
 
     Parameters
     ----------
-    neighbors : np.ndarray
+    neighbors: np.ndarray
         A `n_samples * k_neighbors` matrix containing the indices to each
         points' nearest neighbors in descending order.
-    distances : np.ndarray
+    distances: np.ndarray
         A `n_samples * k_neighbors` matrix containing the distances to the
         neighbors at indices defined in the neighbors parameter.
-    perplexity : double
+    perplexities: double
         The desired perplexity of the probability distribution.
-    symmetrize : bool
+    symmetrize: bool
         Whether to symmetrize the probability matrix or not. Symmetrizing is
         used for typical t-SNE, but does not make sense when embedding new data
         into an existing embedding.
-    n_reference_samples : int
+    n_reference_samples: int
         The number of samples in the existing (reference) embedding. Needed to
         properly construct the sparse P matrix.
-    n_jobs : int
+    n_jobs: int
         Number of threads.
 
     Returns
@@ -179,7 +179,7 @@ def joint_probabilities_nn(neighbors, distances, perplexity, symmetrize=True,
 
     # Compute asymmetric pairwise input similarities
     conditional_P = _tsne.compute_gaussian_perplexity(
-        distances, np.array([perplexity], dtype=float), num_threads=n_jobs)
+        distances, np.array(perplexities, dtype=float), num_threads=n_jobs)
     conditional_P = np.asarray(conditional_P)
 
     P = csr_matrix((conditional_P.ravel(), neighbors.ravel(),
@@ -261,3 +261,70 @@ class FixedSigmaNN(Affinities):
             return P, neighbors, distances
 
         return P
+
+
+class Multiscale(Affinities):
+    def __init__(self, data, perplexities, method='exact', metric='euclidean',
+                 metric_params=None, symmetrize=True, n_jobs=1, random_state=None):
+        self.n_samples = data.shape[0]
+
+        # We will compute the nearest neighbors to the max value of perplexity,
+        # smaller values can just use indexing to truncate unneeded neighbors
+        perplexities = self.check_perplexities(perplexities)
+        max_perplexity = np.max(perplexities)
+        k_neighbors = min(self.n_samples - 1, int(3 * max_perplexity))
+
+        knn_index = build_knn_index(data, method, metric, metric_params, n_jobs, random_state)
+        neighbors, distances = knn_index.query_train(data, k=k_neighbors)
+
+        self.knn_index = knn_index
+        self.P = joint_probabilities_nn(neighbors, distances, perplexities,
+                                        symmetrize=symmetrize, n_jobs=n_jobs)
+
+        self.perplexities = perplexities
+        self.n_jobs = n_jobs
+
+    def to_new(self, data, perplexities=None, return_distances=False):
+        perplexities = perplexities if perplexities is not None else self.perplexities
+        perplexities = self.check_perplexities(perplexities)
+
+        max_perplexity = np.max(perplexities)
+        k_neighbors = min(self.n_samples - 1, int(3 * max_perplexity))
+
+        neighbors, distances = self.knn_index.query(data, k_neighbors)
+
+        P = joint_probabilities_nn(
+            neighbors, distances, perplexities, symmetrize=False,
+            n_reference_samples=self.n_samples, n_jobs=self.n_jobs,
+        )
+
+        if return_distances:
+            return P, neighbors, distances
+
+        return P
+
+    def check_perplexities(self, perplexities):
+        """Check and correct/truncate perplexities.
+
+        If a perplexity is too large, it is corrected to the largest allowed
+        value. It is then inserted into the list of perplexities only if that
+        value doesn't already exist in the list.
+
+        """
+        usable_perplexities = []
+        for perplexity in sorted(perplexities):
+            if 3 * perplexity > self.n_samples - 1:
+                new_perplexity = (self.n_samples - 1) / 3
+
+                if new_perplexity in usable_perplexities:
+                    log.warning('Perplexity value %d is too high. Dropping '
+                                'because the max perplexity is already in the '
+                                'list.' % perplexity)
+                else:
+                    usable_perplexities.append(new_perplexity)
+                    log.warning('Perplexity value %d is too high. Using '
+                                'perplexity %.2f instaed' % (perplexity, new_perplexity))
+            else:
+                usable_perplexities.append(perplexity)
+
+        return usable_perplexities
