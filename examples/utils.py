c37b9542472fbf4a1fb54c9edd32f67eb59bd129
Pavlin Poličar <pavlin.g.p@gmail.com>
2018-12-13 18:09:11 +0100

    Add plotting utils and colors to examples

--- examples/utils.py
+++ examples/utils.py
@@ -7,15 +7,89 @@ from os.path import abspath, dirname, join
 import numpy as np
 
 FILE_DIR = dirname(abspath(__file__))
-DATA_DIR = join(FILE_DIR, 'data')
+DATA_DIR = join(FILE_DIR, "data")
+
+MACOSKO_COLORS = {
+    "Amacrine cells": "#A5C93D",
+    "Astrocytes": "#8B006B",
+    "Bipolar cells": "#2000D7",
+    "Cones": "#538CBA",
+    "Fibroblasts": "#8B006B",
+    "Horizontal cells": "#B33B19",
+    "Microglia": "#8B006B",
+    "Muller glia": "#8B006B",
+    "Pericytes": "#8B006B",
+    "Retinal ganglion cells": "#C38A1F",
+    "Rods": "#538CBA",
+    "Vascular endothelium": "#8B006B",
+}
+ZEISEL_COLORS = {
+    "Astroependymal cells": "#d7abd4",
+    "Cerebellum neurons": "#2d74bf",
+    "Cholinergic, monoaminergic and peptidergic neurons": "#9e3d1b",
+    "Di- and mesencephalon neurons": "#3b1b59",
+    "Enteric neurons": "#1b5d2f",
+    "Hindbrain neurons": "#51bc4c",
+    "Immature neural": "#ffcb9a",
+    "Immune cells": "#768281",
+    "Neural crest-like glia": "#a0daaa",
+    "Oligodendrocytes": "#8c7d2b",
+    "Peripheral sensory neurons": "#98cc41",
+    "Spinal cord neurons": "#c52d94",
+    "Sympathetic neurons": "#11337d",
+    "Telencephalon interneurons": "#ff9f2b",
+    "Telencephalon projecting neurons": "#fea7c1",
+    "Vascular cells": "#3d672d",
+}
+MOUSE_10X_COLORS = {
+    0: "#FFFF00",
+    1: "#1CE6FF",
+    2: "#FF34FF",
+    3: "#FF4A46",
+    4: "#008941",
+    5: "#006FA6",
+    6: "#A30059",
+    7: "#FFDBE5",
+    8: "#7A4900",
+    9: "#0000A6",
+    10: "#63FFAC",
+    11: "#B79762",
+    12: "#004D43",
+    13: "#8FB0FF",
+    14: "#997D87",
+    15: "#5A0007",
+    16: "#809693",
+    17: "#FEFFE6",
+    18: "#1B4400",
+    19: "#4FC601",
+    20: "#3B5DFF",
+    21: "#4A3B53",
+    22: "#FF2F80",
+    23: "#61615A",
+    24: "#BA0900",
+    25: "#6B7900",
+    26: "#00C2A0",
+    27: "#FFAA92",
+    28: "#FF90C9",
+    29: "#B903AA",
+    30: "#D16100",
+    31: "#DDEFFF",
+    32: "#000035",
+    33: "#7B4F4B",
+    34: "#A1C299",
+    35: "#300018",
+    36: "#0AA6D8",
+    37: "#013349",
+    38: "#00846F",
+}
 
 
 def get_zeisel_2018(n_samples: int = None):
-    with gzip.open(join(DATA_DIR, 'zeisel_2018.pkl.gz'), 'rb') as f:
+    with gzip.open(join(DATA_DIR, "zeisel_2018.pkl.gz"), "rb") as f:
         data = pickle.load(f)
 
     # Extract log normalized counts and cell type
-    x, y = data['log_counts'], data['CellType1']
+    x, y = data["log_counts"], data["CellType1"]
     x = x.T
 
     if n_samples is not None:
@@ -26,12 +100,14 @@ def get_zeisel_2018(n_samples: int = None):
 
 
 def get_mnist(n_samples: int = None):
-    if not os.path.exists(join(DATA_DIR, 'mnist.pkl.gz')):
+    if not os.path.exists(join(DATA_DIR, "mnist.pkl.gz")):
         urllib.request.urlretrieve(
-            'http://deeplearning.net/data/mnist/mnist.pkl.gz', join(DATA_DIR, 'mnist.pkl.gz'))
+            "http://deeplearning.net/data/mnist/mnist.pkl.gz",
+            join(DATA_DIR, "mnist.pkl.gz"),
+        )
 
-    with gzip.open(join(DATA_DIR, 'mnist.pkl.gz'), 'rb') as f:
-        train, val, test = pickle.load(f, encoding='latin1')
+    with gzip.open(join(DATA_DIR, "mnist.pkl.gz"), "rb") as f:
+        train, val, test = pickle.load(f, encoding="latin1")
     _train = np.asarray(train[0], dtype=np.float64)
     _val = np.asarray(val[0], dtype=np.float64)
     _test = np.asarray(test[0], dtype=np.float64)
@@ -45,24 +121,83 @@ def get_mnist(n_samples: int = None):
     return x, y
 
 
-def plot(x: np.ndarray, y: np.ndarray, ax=None, draw_legend=True, **kwargs) -> None:
-    if ax is None:
-        import matplotlib.pyplot as plt
-        _, ax = plt.subplots()
+def plot(
+    x,
+    y,
+    ax=None,
+    title=None,
+    draw_legend=True,
+    draw_centers=False,
+    draw_cluster_labels=False,
+    colors=None,
+    **kwargs
+):
+    import matplotlib
 
-    for yi in np.unique(y):
-        mask = y == yi
-        ax.plot(x[mask, 0], x[mask, 1], 'o', label=str(yi),
-                alpha=kwargs.get('alpha', 0.6), ms=kwargs.get('ms', 1))
-    
-    # Hide ticks
-    ax.set_xticks([]), ax.set_yticks([])
-    # Hide box border around figure
-    ax.axis('off')
+    if ax is None:
+        _, ax = matplotlib.pyplot.subplots(figsize=(12, 8))
+
+    if title is not None:
+        ax.set_title(title)
+
+    plot_params = {"alpha": kwargs.get("alpha", 0.6), "s": kwargs.get("s", 1)}
+
+    # Create main plot
+    classes = np.unique(y)
+    if colors is None:
+        default_colors = matplotlib.rcParams["axes.prop_cycle"]
+        colors = {k: v["color"] for k, v in zip(classes, default_colors())}
+
+    point_colors = list(map(colors.get, y))
+
+    ax.scatter(x[:, 0], x[:, 1], c=point_colors, rasterized=True, **plot_params)
+
+    # Plot mediods
+    if draw_centers:
+        centers = []
+        for yi in classes:
+            mask = yi == y
+            centers.append(np.median(x[mask, :2], axis=0))
+        centers = np.array(centers)
+
+        center_colors = list(map(colors.get, classes))
+        ax.scatter(
+            centers[:, 0], centers[:, 1], c=center_colors, s=48, alpha=1, edgecolor="k"
+        )
+
+        # Draw mediod labels
+        if draw_cluster_labels:
+            for idx, label in enumerate(classes):
+                ax.text(
+                    centers[idx, 0],
+                    centers[idx, 1] + 2.1,
+                    label,
+                    fontsize=kwargs.get("fontsize", 6),
+                    horizontalalignment="center",
+                )
+
+    # Hide ticks and axis
+    ax.set_xticks([]), ax.set_yticks([]), ax.axis("off")
 
     if draw_legend:
-        legend = ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-        # Make the legend markers bigger, so we can see what's what
-        for lh in legend.legendHandles:
-            lh._legmarker.set_markersize(6)
-            lh._legmarker.set_alpha(1)
+        legend_handles = [
+            matplotlib.lines.Line2D(
+                [],
+                [],
+                marker="o",
+                color="w",
+                markerfacecolor=colors[yi],
+                ms=7,
+                alpha=1,
+                linewidth=0,
+                label=yi,
+                markeredgecolor="k",
+            )
+            for yi in classes
+        ]
+        ax.legend(
+            handles=legend_handles,
+            loc="center left",
+            bbox_to_anchor=(1, 0.5),
+            frameon=False,
+        )
