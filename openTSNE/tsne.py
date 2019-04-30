Perform gradient clipping for each row individually and fix tests

openTSNE/tsne.py
@@ -609,7 +609,7 @@ class TSNEEmbedding(np.ndarray):
 
     def transform(self, X, perplexity=5, initialization="median", k=25,
                   learning_rate=100, n_iter=100, exaggeration=2, momentum=0,
-                  max_grad_norm=0.25):
+                  max_grad_norm=0.05):
         """Embed new points into the existing embedding.
 
         This procedure optimizes each point only with respect to the existing
@@ -1323,10 +1323,10 @@ class gradient_descent:
             # points overlap with the reference points, leading to large
             # gradients
             if max_grad_norm is not None:
-                norm = np.linalg.norm(gradient)
+                norm = np.linalg.norm(gradient, axis=1)
                 coeff = max_grad_norm / (norm + 1e-6)
-                if coeff < 1:
-                    gradient *= coeff
+                mask = coeff < 1
+                gradient[mask] *= coeff[mask, None]
 
             # Correct the KL divergence w.r.t. the exaggeration if needed
             if should_eval_error and exaggeration != 1:

tests/test_correctness.py
@@ -136,7 +136,12 @@ class TestTSNECorrectness(unittest.TestCase):
             negative_gradient_method="fft",
         ).fit(x_train)
 
-        params = dict(n_iter=100, perplexity=5)
+        # Changing the gradients using clipping changes how the points move
+        # sufficiently so that the interpolation grid is shifted. This test is
+        # more reliable when we don't do gradient clipping and reduce the
+        # learning rate. We increase the number of iterations so that the points
+        # have time to move around
+        params = dict(n_iter=500, perplexity=5, learning_rate=1, max_grad_norm=None)
         # Build up an embedding by adding points one by one
         new_embedding_1 = np.vstack(
             [embedding.transform(np.atleast_2d(point), **params) for point in x_test]
@@ -165,6 +170,7 @@ class TestTSNECorrectness(unittest.TestCase):
             negative_gradient_method="bh",
             early_exaggeration_iter=0,
             n_iter=50,
+            random_state=0,
         ).fit(x_train)
 
         # Evaluate t-SNE optimization using a KNN classifier
@@ -173,7 +179,7 @@ class TestTSNECorrectness(unittest.TestCase):
 
         new_embedding = embedding.transform(x_test, n_iter=100, perplexity=100)
         predictions = knn.predict(new_embedding)
-        self.assertGreater(accuracy_score(predictions, y_test), 0.99)
+        self.assertGreater(accuracy_score(predictions, y_test), 0.95)
 
     def test_iris_fft_transform_correctness(self):
         x_train, x_test, y_train, y_test = train_test_split(
@@ -186,6 +192,7 @@ class TestTSNECorrectness(unittest.TestCase):
             negative_gradient_method="fft",
             early_exaggeration_iter=0,
             n_iter=50,
+            random_state=0,
         ).fit(x_train)
 
         # Evaluate t-SNE optimization using a KNN classifier
@@ -194,7 +201,7 @@ class TestTSNECorrectness(unittest.TestCase):
 
         new_embedding = embedding.transform(x_test, n_iter=100, perplexity=100)
         predictions = knn.predict(new_embedding)
-        self.assertGreater(accuracy_score(predictions, y_test), 0.99)
+        self.assertGreater(accuracy_score(predictions, y_test), 0.95)
 
     def test_bh_transform_with_point_subsets_using_perplexity_nn(self):
         x_train, x_test = train_test_split(