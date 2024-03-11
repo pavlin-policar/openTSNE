388876a43a7b0fcf6d8c0077fa93751190ce8245
PrimozGodec <p.godec9@gmail.com>
2024-03-11 15:03:04 +0100

    Require Python 3.8

--- setup.py
+++ setup.py
@@ -283,7 +283,7 @@ setup(
     ],
 
     packages=setuptools.find_packages(include=["openTSNE", "openTSNE.*"]),
-    python_requires=">=3.7",
+    python_requires=">=3.8",
     install_requires=[
         "numpy>=1.16.6",
         "scikit-learn>=0.20",
